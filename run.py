9b24c7218b29affda3ea7333363d8180635067fa
alfred-nrml <alfrednrml@gmail.com>
2024-12-26 18:33:42 +0530

    Integrated Budgeting and Alerting module into run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, session,fl
 from Expense.app import expense_bp
 from user_reg.main import user_reg_bp
 from Saving.app import saving_bp
-
+from budgeting.budgeting import budgeting_bp  
 def create_app():
     app = Flask(__name__)
 
@@ -19,8 +19,8 @@ def create_app():
     app.register_blueprint(expense_bp, url_prefix='/expense')
     app.register_blueprint(user_reg_bp, url_prefix='/user_reg')
     app.register_blueprint(saving_bp, url_prefix='/saving')
-
-
+    app.register_blueprint(budgeting_bp, url_prefix='/budgeting') 
+    
     return app
 
 if __name__ == "__main__":
